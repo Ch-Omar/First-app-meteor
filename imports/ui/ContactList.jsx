58fede4b0d3b792a9d401af01630f73eb105ba69
Ch-Omar <ch-omar@live.fr>
2023-03-31 14:07:23 +0100

    Using publications, useFind and useSubscribe

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { memo, useState } from "react";
 import { ContactsCollection } from "../api/ContactsCollection";
-import { useTracker } from "meteor/react-meteor-data";
+import { useSubscribe, useFind } from "meteor/react-meteor-data";
 
 export const ContactList = () => {
-  const contacts = useTracker(() => {
-    return ContactsCollection.find({}, { sort: { createdAt: -1 } }).fetch();
-  });
-  const removeContact = (event,_id) => {
-    event.preventDefault();
-    Meteor.call('contacts.remove', {contactId: _id})
+  const [deleted, setDeleted] = useState("");
+
+  const showDeleted = ({ message }) => {
+    setDeleted(message);
+    setTimeout(() => {
+      setDeleted("");
+    }, 5000);
   };
+
+
+  const isloading = useSubscribe("allContacts");
+  const contacts = useFind(() =>
+    ContactsCollection.find({}, { sort: { createdAt: -1 } })
+  );
+  const removeContact = (event, _id) => {
+    event.preventDefault();
+    Meteor.call("contacts.remove", { contactId: _id });
+    setDeleted('Contact deleted')
+    etTimeout(() => {
+      setDeleted("");
+    }, 5000);
+  };
+
+  if (isloading()) {
+    return <h2>Loading</h2>;
+  }
+
+  const ContactItem = memo(({ contact }) => {
+    return (
+      <li
+        className="d-flex justify-content-between"
+        style={{ padding: "10px" }}
+      >
+        <p>{contact.name}</p>
+        <button
+          onClick={(event) => removeContact(event, contact._id)}
+          className="btn btn-danger"
+        >
+          X
+        </button>
+      </li>
+    );
+  });
+
   return (
     <>
+      {deleted && <p style={{ color: "red" }}>{deleted}</p>}
+
       <h2>Contact List</h2>
       <ul className="col-sm-3">
         {contacts.map((contact) => (
-          <li className="d-flex justify-content-between" style={{padding:"10px"}} key={contact._id}>
-            <p>{contact.name}</p>
-            <button onClick={(event) => removeContact(event,contact._id)} class="btn btn-danger">X</button>
-            </li>
+          <ContactItem key={contact._id} contact={contact} />
         ))}
       </ul>
     </>
